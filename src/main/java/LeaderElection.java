a80087f25e0627c95698f648e05b5938064760d5
vgaidarji <veaceslav.gaidarji@gmail.com>
2023-03-03 15:17:20 +0200

    Add Zookeper client connection handling
    Running Zookeper server locally.

---
+++ src/main/java/LeaderElection.java
@@ -0,0 +1,49 @@
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+public class LeaderElection implements Watcher {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private ZooKeeper zookeeper;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        LeaderElection leaderElection = new LeaderElection();
+        leaderElection.connectToZookeeper();
+        leaderElection.run();
+        leaderElection.close();
+        System.out.println("Disconnected from Zookeeper, exiting application.");
+    }
+
+    public void connectToZookeeper() throws IOException {
+        this.zookeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+
+    }
+
+    public void run() throws InterruptedException {
+        synchronized (zookeeper) {
+            zookeeper.wait();
+        }
+    }
+
+    public void close() throws InterruptedException {
+        zookeeper.close();
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        switch (watchedEvent.getType()) {
+            case None:
+                if(watchedEvent.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected");
+                } else {
+                    synchronized (zookeeper) {
+                        System.out.println("Disconnected from Zookeeper");
+                        zookeeper.notifyAll();
+                    }
+                }
+        }
+    }
+}
