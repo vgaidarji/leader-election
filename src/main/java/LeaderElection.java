3fb32d489dcab92c7a0b8c6a2ffa2a694adc9a41
vgaidarji <veaceslav.gaidarji@gmail.com>
2023-03-03 16:04:08 +0200

    Handle nodes lifecycle events

--- src/main/java/LeaderElection.java
+++ src/main/java/LeaderElection.java
@@ -1,58 +1,29 @@
-import java.util.Collection;
-import java.util.Collections;
+import java.io.IOException;
 import java.util.List;
-import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
-
-import java.io.IOException;
+import org.apache.zookeeper.data.Stat;
 
 public class LeaderElection implements Watcher {
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private static final int SESSION_TIMEOUT = 3000;
-    private static final String ELECTION_NAMESPACE = "/election";
+    private static final String TARGET_ZNODE = "/target_znode";
     private ZooKeeper zookeeper;
-    private String currentZnodeName;
 
     public static void main(String[] args) throws IOException, InterruptedException,
             KeeperException {
         LeaderElection leaderElection = new LeaderElection();
         leaderElection.connectToZookeeper();
-        leaderElection.volunteerForLeadership();
-        leaderElection.electLeader();
+        leaderElection.watchTargetZnode();
         leaderElection.run();
         leaderElection.close();
         System.out.println("Disconnected from Zookeeper, exiting application.");
     }
 
-    public void volunteerForLeadership() throws InterruptedException, KeeperException {
-        String znodePrefix = ELECTION_NAMESPACE + "/c_";
-        String znodeFullPath = zookeeper.create(znodePrefix, new byte[]{},
-                ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.EPHEMERAL_SEQUENTIAL);
-
-        System.out.println("znode name" + znodeFullPath);
-        this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
-    }
-
-    public void electLeader() throws InterruptedException, KeeperException {
-        List<String> children = zookeeper.getChildren(ELECTION_NAMESPACE, false);
-        Collections.sort(children);
-        String smallestChild = children.get(0);
-
-        if (smallestChild.equals(currentZnodeName)) {
-            System.out.println("I am the leader");
-            return;
-        }
-        System.out.println("I am not the leader");
-    }
-
     public void connectToZookeeper() throws IOException {
         this.zookeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
-
     }
 
     public void run() throws InterruptedException {
@@ -63,6 +34,18 @@ public class LeaderElection implements Watcher {
 
     public void close() throws InterruptedException {
         zookeeper.close();
+    }
+
+    public void watchTargetZnode() throws InterruptedException, KeeperException {
+        Stat stat = zookeeper.exists(TARGET_ZNODE, this);
+        if (stat == null) {
+            return;
+        }
+
+        byte[] data = zookeeper.getData(TARGET_ZNODE, this, stat);
+        List<String> children = zookeeper.getChildren(TARGET_ZNODE, this);
+
+        System.out.println("Data: " + new String(data) + " children : " + children);
     }
 
     @Override
@@ -77,6 +60,25 @@ public class LeaderElection implements Watcher {
                         zookeeper.notifyAll();
                     }
                 }
+                break;
+            case NodeDeleted:
+                System.out.println(TARGET_ZNODE + " was deleted");
+                break;
+            case NodeCreated:
+                System.out.println(TARGET_ZNODE + " was created");
+                break;
+            case NodeDataChanged:
+                System.out.println(TARGET_ZNODE + " data changed");
+                break;
+            case NodeChildrenChanged:
+                System.out.println(TARGET_ZNODE + " children changed");
+                break;
+        }
+
+        try {
+            watchTargetZnode();
+        } catch (InterruptedException | KeeperException e) {
+            throw new RuntimeException(e);
         }
     }
 }
